c6aab749acd5944df2c961f41ebad357bc0bc1bf
ashishkunthe <ashishkunthe2003@gmail.com>
2025-07-09 20:31:07 +0530

    ..

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { createBlog, getBlogs } from "../controllers/blogController";
+import { createBlog, getBlog, getBlogs } from "../controllers/blogController";
 
 const routes = Router();
 
 routes.post("/blogs", authMiddleware, createBlog as any);
 routes.get("/blogs", authMiddleware, getBlogs);
-routes.get("/blogs/:id");
+routes.get("/blogs/:id", getBlog as any);
 
 export default routes;
